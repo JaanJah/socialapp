03f0c18d2daa1e3d0c955ec11fb4f1292fdac6bd
JaanJahimees <jaanjahilo@outlook.com>
2018-10-29 09:49:40 +0200

    Initial Commit

---
+++ socialapp/CustomAdapter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace socialapp
+{
+    class CustomAdapter : BaseAdapter<Properties>
+    {
+        List<Properties> items;
+        Activity context;
+
+
+        public CustomAdapter(Activity context, List<Properties> items) : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+
+        public override Properties this[int position]
+        {
+            get { return items[position]; }
+        }
+
+        public override int Count { get { return items.Count; } }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            if (view == null)
+                view = context.LayoutInflater.Inflate(Resource.Layout.CustomRow, null);
+
+            view.FindViewById<TextView>(Resource.Id.userName).Text = items[position].Owner;
+            view.FindViewById<TextView>(Resource.Id.userMessage).Text = items[position].Message;
+            view.FindViewById<TextView>(Resource.Id.msgComments).Text = items[position].Comments + " Comments";
+            view.FindViewById<TextView>(Resource.Id.msgLikes).Text = items[position].Likes + " Likes";
+            view.FindViewById<ImageView>(Resource.Id.userIcon);
+            view.FindViewById<ImageView>(Resource.Id.msgLikeIcon);
+
+            return view;
+
+        }
+    }
+}
---
+++ socialapp/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace socialapp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ListView list;
+        List<Properties> properties;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            list = FindViewById<ListView>(Resource.Id.listView1);
+            properties = new List<Properties>();
+            properties.Add(
+                new Properties
+                {
+                    Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae sem nibh. Aliquam in ornare tellus. Aenean vel luctus metus. Fusce turpis est, euismod at hendrerit at, mollis rhoncus sem. Morbi mauris odio, fermentum non massa et, semper consectetur enim. Nulla ornare a urna pellentesque aliquam. In accumsan arcu vitae turpis malesuada, vel molestie est venenatis.",
+                    Owner = "Jaan Markus",
+                    Likes = 5,
+                    Comments = 5
+                }
+            );
+            list.Adapter = new CustomAdapter(this, properties);
+
+        }
+    }
+}
